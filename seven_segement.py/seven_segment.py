45106f4a0027a1cb230d9c1e3d8d5eb3f7b12813
Al-lero <aleroutieyione@gmail.com>
2024-04-27 19:18:07 +0100

    seven_segement

---
+++ seven_segement.py/seven_segment.py
@@ -0,0 +1,33 @@
+number = int(input("Enter number to display: "))
+
+
+
+if number == 0:
+	print(" _\n| |\n|_|")
+
+if number == 1:
+	print(" | \n |   ")
+
+if number == 2:
+	print(" _ \n _|\n|_")
+
+if number == 3:
+	print("_\n_|\n_|")
+
+if number == 4:
+	print("|  |\n _\n   |")
+
+if number == 5:
+	print(" _\n|_\n _|")
+
+if number == 6:
+	print(" _\n|_\n|_|")
+
+if number == 7:
+	print("_\n | \n |")
+
+if number == 8:
+	print(" _\n|_|\n|_|")
+
+if number == 9:
+	print(" _\n|_|\n  |")
